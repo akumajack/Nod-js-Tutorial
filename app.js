ba5e23194962d3d6e05237c88076323aff06ea41
akumajack <api2557wan@gmail.com>
2022-07-06 17:36:09 +0700

    update code

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 //const debug = require('debug')('app');
 const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/",(req,res) =>{
 
